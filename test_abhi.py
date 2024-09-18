00c5d1b2b8fca87a1ad2a17f10ef1fadea4f2c0a
Abhilash-Jagityla <abhilash.jagityla@gmail.com>
2024-09-18 14:57:22 +0100

    name changed

---
+++ test_abhi.py
@@ -0,0 +1,15 @@
+
+def buy_dough():
+    dough_quantity = 5
+    return dough_quantity
+
+def bake_pizza():
+    make_dough = buy_dough();
+    print(make_dough)
+
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == '__main__':
+    q = buy_dough()
+    print(q)
