ca8f3518c6eaa6923377d7a1c2f3b3d113d87f18
Abhilash Jagityla <abhilash.jagityla@gmail.com>
2024-10-29 16:36:59 +0000

    Setup and demo pymongo

---
+++ pymongo_demo.py
@@ -0,0 +1,24 @@
+import pymongo
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+client = pymongo.MongoClient(os.getenv("MONGODB_CONNECTION_STRING"))
+
+db = client.todoappdatabase
+
+collection = db.todo_items
+
+# Insert  a record
+collection.insert_one({"description": "A second pymongo document"})
+
+# Read a record
+list (collection.find())
+
+# Insert and update a record
+collection.insert_one({"description": "An updateable document", "type": "updateable"})
+collection.update_one({"type": "updateable"}, {"$set" : {"description": "CHANGED!!!"}})
+
+
+print(collection)
