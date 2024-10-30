193578193cda3772ca7ae3188830f4db2c8dafd9
Abhilash Jagityla <abhilash.jagityla@gmail.com>
2024-10-30 15:21:55 +0000

    Trello removed and connected to mongo db- run flask run

---
+++ todo_app/data/mongo_items.py
@@ -0,0 +1,21 @@
+import pymongo
+import os
+
+client = pymongo.MongoClient(os.getenv("MONGODB_CONNECTION_STRING"))
+
+db = client[os.getenv("MONGODB_DATABASE_NAME")]
+
+collection = db[os.getenv("MONGODB_COLLECTION_NAME")]
+
+
+def add_item(new_todo_title: str):
+    new_items = {
+        "name": new_todo_title,
+        "status": "TO DO"
+    }
+def get_items(): 
+    items = []
+    return items
+    pass
+def move_item_to_done():
+    pass
